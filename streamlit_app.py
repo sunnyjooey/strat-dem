ea2fdcabcab79aacdd53886aaaae6ec26e2db297
sunnyjooey@gmail.com <sunnyjooey@gmail.com>
2025-08-29 17:01:51 +0000

    pw

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,44 @@
 import streamlit as st
 import pandas as pd
+import hmac
 
-st.title("Strategies for Democracy")
-st.write(
-    "By Rob Hager"
-)
 
-df = pd.read_csv("stratdem.csv")
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-# Select which columns to display
-col1_val = st.selectbox("Select a chapter", df.chapter)
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
 
-# Find the corresponding value(s) in column2
-results = df.loc[df["chapter"] == col1_val, "summary"]
+    if st.session_state.get("password_correct", False):
+        return True
 
-st.write("##### Summary")
-for r in results:
-    st.write(r)
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("Incorrect password.")
+    return False
+
+if check_password():
+
+    st.title("Strategies for Democracy")
+    st.write(
+        "By Rob Hager"
+    )
+
+    df = pd.read_csv("dt/stratdem.csv")
+
+    # Select which columns to display
+    col1_val = st.selectbox("Select a chapter", df.chapter)
+
+    # Find the corresponding value(s) in column2
+    results = df.loc[df["chapter"] == col1_val, "summary"]
+
+    st.write("##### Summary")
+    for r in results:
+        st.write(r)
